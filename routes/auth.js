ddf6db5b679fda27f72b2242b8b30e55032cda62
arnabuchiha <arnabkeya04@gmail.com>
2020-05-23 21:32:04 +0530

    Login created

--- routes/auth.js
+++ routes/auth.js
@@ -1,8 +1,8 @@
 const router=require('express').Router();
 const User=require('../model/User');
-const {registerValidation}=require('../validation');
+const {registerValidation,loginValidation}=require('../validation');
 const bcrypt=require('bcryptjs');
-
+const jwt=require('jsonwebtoken');
 
 router.post('/register',async(req,res)=>{
     const {error}=registerValidation(req.body);
@@ -22,11 +22,25 @@ router.post('/register',async(req,res)=>{
     });
     try{
         const savedUser=await user.save();
-        res.send(savedUser)
+        res.json({success:true});
     }catch(err){
         res.status(400).send(err);
     }
 
 })
+
+
+router.post('/login',async (req,res)=>{
+    const {error}=loginValidation(req.body);
+    if(error)return res.status(400).send(error.details[0].message);
+    const user=await User.findOne({email:req.body.email});
+    if(!user) return res.status(400).send('Email or Password is wrong');
+    const validPass=await bcrypt.compare(req.body.password,user.password);
+    if(!validPass)return res.status(400).send('Email or Password is wrong');
+    
+    const token=jwt.sign({_id:user._id},process.env.TOKEN_SECRET);
+    res.header('auth-token',token).send(token);
+
+});
 
 module.exports=router;
