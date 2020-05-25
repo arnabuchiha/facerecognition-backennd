9aebd518306f22adf550524aa8b60babf9d1b151
arnabuchiha <arnabkeya04@gmail.com>
2020-05-25 11:31:01 +0530

    Deploy

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const express=require('express');
+var cors = require('cors')
 const app=express();
 const dotenv=require('dotenv')
 const mongoose=require('mongoose');
 
 dotenv.config();
-
+app.use(cors());
 mongoose.connect(process.env.DB_CONNECT,
 {useNewUrlParser:true},
 ()=>console.log('Connected to db'))
@@ -15,4 +16,4 @@ app.use(express.json());
 
 app.use('/api/user',authRoute);
 app.use('/api/rank',rankRoute);
-app.listen(3000,()=>console.log('Up and Running'));
+app.listen(process.env.PORT,()=>console.log('Up and Running'));
