e0b6a2b6b0f38b5096be2dcd6fb4ae032c13d2f3
arnabuchiha <arnabkeya04@gmail.com>
2020-05-25 23:32:28 +0530

    Verify token fixed

--- routes/rank.js
+++ routes/rank.js
@@ -8,12 +8,12 @@ router.get('/',verify,async (req,res)=>{
 router.post('/',verify,async(req,res)=>{
     const user=await User.findOne({_id:req.user._id})
     var rank=user.rank
-    await User.findByIdAndUpdate(req.user._id,{"rank":rank+=1},function(err,result){
+    await User.findByIdAndUpdate(req.user._id,{"rank":rank+1},function(err,result){
         if(err){
             res.send(err);
         }
         else{
-            res.send({success:true});
+            res.send({success:true,rank:rank+1});
         }
     })
 })
