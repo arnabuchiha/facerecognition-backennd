48736d536918a7935004c07c9283c0189abaf790
arnabuchiha <arnabkeya04@gmail.com>
2020-05-25 23:25:10 +0530

    Verify token fixed

--- routes/verifyToken.js
+++ routes/verifyToken.js
@@ -1,6 +1,7 @@
 const jwt=require('jsonwebtoken');
 
 module.exports=function(req,res,next){
+    const token=req.header('auth-token');
 
     try{
         const verified=jwt.verify(token,process.env.TOKEN_SECRET);
