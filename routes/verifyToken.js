26f55703a2c7c13100db35aa930596b9ac917a9e
arnabuchiha <arnabkeya04@gmail.com>
2020-05-25 23:22:20 +0530

    Verify token fixed

--- routes/verifyToken.js
+++ routes/verifyToken.js
@@ -1,8 +1,6 @@
 const jwt=require('jsonwebtoken');
 
 module.exports=function(req,res,next){
-    const token=req.header('auth-token');
-    if(!token)return res.send('Access Denied');
 
     try{
         const verified=jwt.verify(token,process.env.TOKEN_SECRET);
